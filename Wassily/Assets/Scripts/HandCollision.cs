8240f69ac3a8e2475c95aec7bee39d9c57d1b991
notthewarriorprincess <notthewarriorprincess@gmail.com>
2017-03-12 14:21:10 -0600

    (probably broken code)

--- Wassily/Assets/Scripts/HandCollision.cs
+++ Wassily/Assets/Scripts/HandCollision.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class HandCollision : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    bool triOn = false;
+    bool gridOrb = false;
+
 	void Start () {
 		
 	}
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Triangle1"))
+        if ( !triOn && other.gameObject.CompareTag("Triangle1"))
         {
-            other.gameObject.SetActive(false);
+            triOn = true;
+            other.gameObject.transform.Rotate(new Vector3(1, 30, 45) * Time.deltaTime);
         }
-    }
+
+        if (triOn && other.gameObject.CompareTag("Triangle1"))
+        {
+            triOn = false;
+            other.gameObject.transform.Rotate(new Vector3());
+        }
+
+        if (!gridOrb && other.gameObject.CompareTag("gridsphere"))
+        {
+            gridOrb = true;
+            while (gridOrb)
+            {
+                float moveHorizontal = Input.GetAxis("Horizontal");
+                float moveVertical = Input.GetAxis("Vertical");
+                Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+                movement =  new Vector3(0,0,0);
+
+            }
+
+
+        }
 
     // Update is called once per frame
     void Update () {
