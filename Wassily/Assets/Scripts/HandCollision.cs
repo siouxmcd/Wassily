24dc693fbadaf9dff71d3fb580625b113cd3fe20
notthewarriorprincess <notthewarriorprincess@gmail.com>
2017-03-12 13:19:28 -0600

    Collision testing

---
+++ Wassily/Assets/Scripts/HandCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Triangle1"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
